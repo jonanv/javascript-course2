d7a39a0805701055716581c332710eab169c038a
jonanv <jonan-vargas23@hotmail.com>
2025-05-09 12:33:13 -0500

    :art: (variables): Add variables with const

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,11 @@
+const producto = "Monitor de 20 pulgadas";
+
+// No se puede reasignar una variable const
+producto = "Tablet";
+
+console.log(producto);
+
+
+// No se puede declarar una variable const sin inicializarla
+const precio;
+console.log(precio);
