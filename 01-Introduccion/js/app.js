06a275fe9a321eb1d4c61db3d20d7a5f07e9a3da
jonanv <jonan-vargas23@hotmail.com>
2025-05-09 11:06:22 -0500

    :art: (introduction): Add comments line and block

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,5 +1,7 @@
-console.log('Hola Mundo JavasScript');
+// console.log('Hola Mundo JavasScript');
 
+/* 
 const nombre = prompt('¿Cuál es tu nombre?');
 const contenido = document.querySelector('.contenido');
 contenido.innerHTML = `${ nombre } esta aprendiendo JavaScript Moderno`;
+ */
