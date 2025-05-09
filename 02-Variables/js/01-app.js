2258179937b349f01b98f0cb85807f099d87cdac
jonanv <jonan-vargas23@hotmail.com>
2025-05-09 12:20:07 -0500

    :art: (variables): Add variables with var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,36 @@
+// Declarar e inicializar una variable con un valor
+var producto = "Monitor 24 pulgadas";
+console.log(producto);
+
+// Las variables pueden ser reasignadas
+producto = "Monitor 19 pulgadas";
+console.log(producto);
+
+// Javascript es un lenguaje de tipado dinámico, no es necesario declarar el tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se puede declarar un valor sin inicializar e inicializar después
+var disponible;
+disponible = true;
+disponible = false;
+
+// Inicializar multiples variables
+var categoria = "Computadoras",
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+// No se puede iniciar una variable con un número
+var 99dias;
+var dias99;
+
+var 01_;
+var _01;
+var _usuario;
+var __usuario;
+
+// 
+var nombreproducto;
+var nombreProducto; // camelCase
+var nombre_producto; // snake_case
+var NombreProducto; // PascalCase
