21058f38254f4cecfac004f4cfba7ec598e6b79f
jonanv <jonan-vargas23@hotmail.com>
2025-05-09 12:28:20 -0500

    :art: (variables): Add variables with let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,16 @@
+let producto = "Monitor 20 pulgadas";
+
+// Reasignar el valor de la variable
+producto = "Tablet";
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+let precio;
+
+precio = 300;
+
+console.log(precio);
