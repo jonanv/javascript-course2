37aa834a5473ed90a241762dbf1993bbc7291be7
jonanv <jonan-vargas23@hotmail.com>
2025-05-09 10:50:35 -0500

    :art: (introduction): Add promt to javascript

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,5 @@
 console.log('Hola Mundo JavasScript');
+
+const nombre = prompt('¿Cuál es tu nombre?');
+const contenido = document.querySelector('.contenido');
+contenido.innerHTML = `${ nombre } esta aprendiendo JavaScript Moderno`;
