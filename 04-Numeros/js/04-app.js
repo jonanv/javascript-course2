0329d7bb51b68a5f221198ba233fbc8c9ae32a19
jonanv <jonan-vargas23@hotmail.com>
2025-05-10 18:08:56 -0500

    :art: (numbers): Add order to operations

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,23 @@
+// Orden de operaciones
+
+let resultado;
+
+// 1. Paréntesis
+resultado = (2 + 3) * 4; // 20
+console.log(resultado);
+
+// 2. Exponentes
+resultado = 2 + 2 ** 3; // 10
+console.log(resultado);
+
+// 3. Multiplicación y división
+resultado = 2 + 3 * 4; // 14
+console.log(resultado);
+
+// 4. Suma y resta
+resultado = 2 + 3 - 4; // 1
+console.log(resultado);
+
+// 5. Concatenación
+resultado = 2 + '3' + 4; // 234
+console.log(resultado);
