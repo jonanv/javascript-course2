e575d7d4c5283ffeac690009a27d0b55cb55a2e7
jonanv <jonan-vargas23@hotmail.com>
2025-05-10 16:47:56 -0500

    :art: (strings): Add methods toUpperCase and toLowerCase

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,9 @@
+const producto = "Monitor 20 Pulgadas ";
+
+console.log(producto);
+
+// Convirtir a mayúsculas
+console.log(producto.toUpperCase());
+
+// Convirtir a minúsculas
+console.log(producto.toLowerCase());
