91f771884f452a8081ecc08062f54b70caef63f9
jonanv <jonan-vargas23@hotmail.com>
2025-05-10 18:00:23 -0500

    :art: (numbers): Add methods of object Math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,46 @@
+// Objecto Math
+
+let resultado;
+
+// PI
+resultado = Math.PI;
+console.log(resultado);
+
+// Redondear
+resultado = Math.round(2.4);
+console.log(resultado);
+
+resultado = Math.round(2.5);
+console.log(resultado);
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.4);
+console.log(resultado);
+
+// Redondear hacia abajo
+resultado = Math.floor(2.4);
+console.log(resultado);
+
+// Raiz cuadrada
+resultado = Math.sqrt(144);
+console.log(resultado);
+
+// Potencia
+resultado = Math.pow(8, 2);
+console.log(resultado);
+
+// Valor absoluto
+resultado = Math.abs(-200);
+console.log(resultado);
+
+// Minimo
+resultado = Math.min(3, 5, 1, 8, -2);
+console.log(resultado);
+
+// Maximo
+resultado = Math.max(3, 5, 1, 8, -2);
+console.log(resultado);
+
+// Aleatorio
+resultado = Math.random();
+console.log(resultado);
