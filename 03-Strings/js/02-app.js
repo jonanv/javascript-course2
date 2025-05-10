7803fbeb1d02ef89e2de3a766587a351b336f4d4
jonanv <jonan-vargas23@hotmail.com>
2025-05-10 16:02:03 -0500

    :art: (strings): Add methods of strings

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,13 @@
+const producto = 'Monitos 20 pulgadas';
+
+console.log(producto);
+
+// Conocer la longitud de una cadena
+console.log(producto.length);
+
+// Conocer la posición de una cadena
+console.log(producto.indexOf('pulgadas'));
+
+// Conocer si una cadena contiene otra cadena
+console.log(producto.includes('pulgadas')); // true
+console.log(producto.includes('Pulgadas')); // false
