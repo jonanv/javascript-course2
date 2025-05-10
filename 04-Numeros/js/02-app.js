82ef899b39b63f1b6c8c49791887d78eb120f96b
jonanv <jonan-vargas23@hotmail.com>
2025-05-10 17:51:48 -0500

    :art: (numbers): Add operators for numbers

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,40 @@
+// Creación de números
+
+const numero1 = 30;
+const numero2 = 20;
+
+let resultado;
+
+// Suma
+resultado = numero1 + numero2;
+console.log('Suma: ', resultado);
+
+// Resta
+resultado = numero1 - numero2;
+console.log('Resta: ', resultado);
+
+// Multiplicación
+resultado = numero1 * numero2;
+console.log('Multipliación: ', resultado);
+
+// División
+resultado = numero1 / numero2;
+console.log('División: ', resultado);
+
+// Módulo
+resultado = numero1 % numero2;
+console.log('Módulo: ', resultado);
+
+// Potencia
+resultado = numero1 ** 2;
+console.log('Potencia: ', resultado);
+
+// Incremento
+let numero3 = 10;
+numero3++;
+console.log('Incremento: ', numero3);
+
+// Decremento
+let numero4 = 10;
+numero4--;
+console.log('Decremento: ', numero4);
