db14c21814ef7b082a85d344c68d68bccbca0aa4
jonanv <jonan-vargas23@hotmail.com>
2025-05-10 18:24:02 -0500

    :art: (numbers): Add increases and decreases

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,30 @@
+// Incremento y Decremento
+
+let resultado;
+
+// Incremento
+resultado = 10;
+resultado++; // resultado = resultado + 1
+console.log(resultado); // 11
+
+// Decremento
+resultado = 10;
+resultado--; // resultado = resultado - 1
+console.log(resultado); // 9
+
+// Incremento y Decremento en una sola línea
+resultado = 10;
+resultado += 5; // resultado = resultado + 5
+console.log(resultado); // 15
+
+// Preincremento y Postincremento
+resultado = 10;
+console.log(++resultado); // 11 (preincremento)
+console.log(resultado++); // 11 (postincremento)
+console.log(resultado); // 12 (resultado después del postincremento)
+
+// Predecremento y Postdecremento
+resultado = 10;
+console.log(--resultado); // 9 (predecremento)
+console.log(resultado--); // 9 (postdecremento)
+console.log(resultado); // 8 (resultado después del postdecremento)
