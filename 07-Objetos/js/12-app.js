c454bacdf3732df91a4bdeb0da2cc088df78f1d7
jonanv <jonan-vargas23@hotmail.com>
2025-05-11 17:08:29 -0500

    :art: (objects): Object literal

--- 07-Objetos/js/12-app.js
+++ 07-Objetos/js/12-app.js
@@ -1,13 +1,13 @@
 // Construcción de objetos
 
 // Object literal
-// const persona = {
-//     nombre: 'John',
-//     edad: 30,
-//     hablar: function() {
-//         console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
-//     }
-// }
+const persona = {
+    nombre: 'John',
+    edad: 30,
+    hablar: function() {
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    }
+}
 
 // Object constructor
 function Persona(nombre, edad) {
@@ -18,5 +18,5 @@ function Persona(nombre, edad) {
     }
 }
 
-const persona = new Persona('John', 30);
-console.log(persona);
+const persona1 = new Persona('John', 30);
+console.log(persona1);
