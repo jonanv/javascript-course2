b27ed35d5b6198f115be5eda59fd04d8f0672726
jonanv <jonan-vargas23@hotmail.com>
2025-05-12 11:39:44 -0500

    :art: (arrays): Add elements start and end of array

---
+++ 08-Arrays/js/05-app.js
@@ -0,0 +1,14 @@
+// Añadir nuevos elementos al final o inicio de un array
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+console.table(meses);
+
+// push
+// Agrega un nuevo elemento al final del array
+meses.push('Julio');
+console.table(meses);
+
+// unshift
+// Agrega un nuevo elemento al inicio del array
+meses.unshift('Enero');
+console.table(meses);
