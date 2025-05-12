6b23bbe612a0f1b9224b8990dbd97d706da7e83a
jonanv <jonan-vargas23@hotmail.com>
2025-05-12 11:31:57 -0500

    :art: (arrays): Add elements of arrays

---
+++ 08-Arrays/js/04-app.js
@@ -0,0 +1,10 @@
+// Asignar nuevos valores a un array
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+
+console.table(meses);
+
+meses[0] = 'Enero 2023';
+meses[10] = 'Diciembre 2023';
+
+console.table(meses);
