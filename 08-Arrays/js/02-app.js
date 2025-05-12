fe0c71d8d66b85548b0f6c25ce83ab1de53a9dde
jonanv <jonan-vargas23@hotmail.com>
2025-05-12 11:03:08 -0500

    :art: (arrays): Access to element of array

---
+++ 08-Arrays/js/02-app.js
@@ -0,0 +1,14 @@
+// Acceder a los elementos de un array
+
+const numbers = [10, 20, 30, 40, 50];
+
+console.table(numbers);
+
+// Acceder al primer elemento
+console.log(numbers[0]); // 1
+// Acceder al segundo elemento
+console.log(numbers[1]); // 2
+// Acceder al tercer elemento
+console.log(numbers[2]); // 3
+// Acceder al cuarto elemento
+console.log(numbers[3]); // 4
