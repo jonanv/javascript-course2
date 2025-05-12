a6e271636b47a484d4c587f8dd3bf872a70925f6
jonanv <jonan-vargas23@hotmail.com>
2025-05-12 14:57:33 -0500

    :sparkles: (functions): Hoisting, diference between functions expressions and functions declarations

---
+++ 09-Funciones/js/02-app.js
@@ -0,0 +1,22 @@
+// Diferencias entre funtion expression y function declaration
+
+// Function Expression
+// Function expression NO se pueden llamar antes de ser declaradas
+sumar(2, 3);
+
+const sumar = function (a, b) {
+    console.log(a + b);
+};
+
+// Function Declaration
+// Function declaration se pueden llamar antes de ser declaradas
+restar(5, 2);
+
+function restar(a, b) {
+    console.log(a + b);
+}
+
+// La diferencia consiste en el hoisting
+// El hoisting es un comportamiento de JavaScript en el que las declaraciones de funciones y 
+// variables son elevadas a la parte superior de su contexto de ejecución
+// En el caso de las function declarations, el hoisting permite que la función sea llamada antes de su declaración
