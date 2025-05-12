efb84b39a3355e9413d3cc7b6744d1785b69b289
jonanv <jonan-vargas23@hotmail.com>
2025-05-12 15:14:11 -0500

    :art: (functions): Arguments and parameters

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,9 @@
+// Parámetros y argumentos en funciones
+
+// Parámetros: son las variables que se definen en la declaración de la función
+function saludar(nombre) {
+    console.log(`Hola ${nombre}`);
+}
+
+// Argumentos: son los valores que se pasan a la función al momento de invocarla
+saludar('Juan'); // Hola Juan
