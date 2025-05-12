9b7688a7b1c7b92d92a1744c5e169204a4dc2127
jonanv <jonan-vargas23@hotmail.com>
2025-05-12 11:45:21 -0500

    :art: (arrays): Add element with spread operator

---
+++ 08-Arrays/js/06-app.js
@@ -0,0 +1,6 @@
+// Crear un nuevo arreglo con el spread operator
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+const meses2 = [...meses, 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+console.table(meses2);
