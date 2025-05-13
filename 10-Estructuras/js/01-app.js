bd61276cd1357403334c8c22d69daef51968b8fb
jonanv <jonan-vargas23@hotmail.com>
2025-05-13 18:43:24 -0500

    :art: (control-structures): Strict comparator

--- 10-Estructuras/js/01-app.js
+++ 10-Estructuras/js/01-app.js
@@ -2,7 +2,7 @@
 
 const puntaje = 1000;
 
-if (puntaje === 1000) {
+if (puntaje == 1000) {
     console.log("El puntaje es 1000");
 } else {
     console.log("El puntaje no es 1000");
---
+++ 10-Estructuras/js/02-app.js
@@ -0,0 +1,15 @@
+// Comparador estricto
+
+const puntaje = 1000;
+
+if (puntaje != 1000) {
+    console.log("El puntaje no es 1000");
+} else {
+    console.log("El puntaje es 1000");
+}
+
+if (puntaje === "1000") {
+    console.log("El puntaje no es '1000'");
+} else {
+    console.log("El puntaje es 1000");
+}
