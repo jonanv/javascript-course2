e8eaec54bd80b0ada77b7da764289e6c24e94f6e
jonanv <jonan-vargas23@hotmail.com>
2025-05-14 17:19:40 -0500

    :art: (dom): Access to elements of DOM

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,18 @@
+let element;
+
+element = document;
+element = document.head;
+element = document.body;
+element = document.location;
+element = document.URL;
+element = document.forms;
+element = document.forms[0].id;
+element = document.forms[0].method;
+element = document.forms[0].action;
+element = document.links;
+
+element = document.images;
+
+element = document.scripts;
+
+console.log(element);
