bcf0e330df75d50e68547ff3a316063e3ea20be6
jonanv <jonan-vargas23@hotmail.com>
2025-05-13 19:00:51 -0500

    :art: (control-structures): Switch

---
+++ 10-Estructuras/js/05-app.js
@@ -0,0 +1,24 @@
+// Switch para evaluar multiples condiciones
+
+let dia = 5;
+
+switch (dia) {
+    case 1:
+        console.log("Lunes");
+        break;
+    case 2:
+        console.log("Martes");
+        break;
+    case 3:
+        console.log("Miercoles");
+        break;
+    case 4:
+        console.log("Jueves");
+        break;
+    case 5:
+        console.log("Viernes");
+        break;
+    default:
+        console.log("Fin de semana");
+        break;
+}
