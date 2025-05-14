ba1373ec8f916baaf0977d4cdfdb916629de44f3
jonanv <jonan-vargas23@hotmail.com>
2025-05-14 16:30:11 -0500

    :sparkles: (array-methods): Filter

---
+++ 12-ArrayMethods/js/04-app.js
@@ -0,0 +1,18 @@
+// filter
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+];
+
+const resultado1 = meses.filter(mes => mes.length > 6);
+console.log(resultado1); // ['Febrero']
+
+const resultado2 = carrito.filter(producto => producto.precio > 400);
+console.log(resultado2);
