2083e38549700a25c087d6e66a178ca4c3407a14
jonanv <jonan-vargas23@hotmail.com>
2025-05-14 16:37:10 -0500

    :sparkles: (array-methods): Concant

---
+++ 12-ArrayMethods/js/07-app.js
@@ -0,0 +1,15 @@
+// concat
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+
+const carrito = [
+    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Televisión', precio: 100 },
+    { nombre: 'Tablet', precio: 200 },
+    { nombre: 'Audifonos', precio: 300 },
+    { nombre: 'Teclado', precio: 400 },
+    { nombre: 'Celular', precio: 700 },
+];
+
+const resultado = meses.concat(carrito);
+console.log(resultado);
