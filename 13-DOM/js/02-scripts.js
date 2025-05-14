5dd243ce8412a2260f84fdf94be5b442c039ed6b
jonanv <jonan-vargas23@hotmail.com>
2025-05-14 17:28:52 -0500

    :art: (dom): Get elements by class name

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,7 @@
+// elementos por su clase
+
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+const header2 = document.querySelector('.header');
+console.log(header2);
