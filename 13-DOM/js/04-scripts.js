df0d4987293901b86f0d402b1129334acf277af4
jonanv <jonan-vargas23@hotmail.com>
2025-05-14 17:51:17 -0500

    :art: (dom): Query selector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,16 @@
+// querySelector
+
+const card = document.querySelector('.card');
+console.log(card);
+
+const info = document.querySelector('.premium .info');
+console.log(info);
+
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)');
+console.log(segundoCard);
+
+const formulario = document.querySelector('#formulario');
+console.log(formulario);
+
+const nav = document.querySelector('nav');
+console.log(nav);
