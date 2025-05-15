cfd134c9612cb6aafe7f05dd841f128c53d62046
jonanv <jonan-vargas23@hotmail.com>
2025-05-14 19:27:18 -0500

    :art: (dom): Query selector all

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,7 @@
+// querySelectorAll
+
+const allLinks = document.querySelectorAll('a');
+console.log(allLinks);
+
+const noExiste = document.querySelectorAll('.no-existe');
+console.log(noExiste); // NodeList(0) []
