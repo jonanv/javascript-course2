98af634f4c8a15ab209db3bdfecbea5f646e3bd8
jonanv <jonan-vargas23@hotmail.com>
2025-05-19 20:50:56 -0500

    :art: (project-car): Define variables and event listener add to car

---
+++ 15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,17 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    listaCursos.addEventListener('click', agregarCurso);
+}
+
+// Funciones
+function agregarCurso(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+    }
+}
