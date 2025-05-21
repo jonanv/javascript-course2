a24410691230c7039fa840264623502cc189b4a1
jonanv <jonan-vargas23@hotmail.com>
2025-05-20 19:04:44 -0500

    :art: (project-car): Show de information to car

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -28,6 +28,7 @@ function leerDatosCurso(curso) {
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
     }
+    // console.log(infoCurso);
 
     // Revisa si un elemento ya existe en el carrito
     const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
@@ -48,4 +49,27 @@ function leerDatosCurso(curso) {
     }
 
     console.log(articulosCarrito);
+    carritoHTML();
+function carritoHTML() {
+    // Limpiar el HTML
+    limpiarHTML();
+
+    // Recorre el carrito y genera el HTML
+    articulosCarrito.forEach(curso => {
+        const { imagen, titulo, precio, cantidad, id } = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width=100>
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}">X</a>
+            </td>
+        `;
+        contenedorCarrito.appendChild(row);
+    });
+}
 }
