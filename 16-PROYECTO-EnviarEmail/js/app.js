96e3e39c9d383882da6a81780949aa4b37041363
jonanv <jonan-vargas23@hotmail.com>
2025-05-21 19:47:11 -0500

    :art: (project-email): Add eventListener blur to field email

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Seleccionar elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+
+    // Asignar eventos
+    inputEmail.addEventListener('blur', function(e) {
+        console.log(e.target.value);
+    });
 });
