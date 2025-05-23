85166daa2122bc3bd80305a390424dcf836c2010
jonanv <jonan-vargas23@hotmail.com>
2025-05-22 19:57:11 -0500

    :art: (project-email): Add functions mostrarError, limpiarError and validarEmail

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -12,6 +12,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Funciones
     function validar(e) {
-        console.log(e.target.value);
+        const element = e.target;
+        if (e.target.value.trim() === '') {
+            mostrarError(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+            element.style.borderColor = 'red';
+            return;
+        }
+
+        if (e.target.type === 'email') {
+            if (!validarEmail(e.target.value)) {
+                mostrarError('Email no válido', e.target.parentElement);
+                element.style.borderColor = 'red';
+                return;
+            }
+        }
+
+        element.style.borderColor = 'green';
+        limpiarError(e.target.parentElement);
+    }
+
+    function mostrarError(mensaje, referencia) {
+        limpiarError(referencia);
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add('error');
+        error.style.color = 'red';
+        referencia.appendChild(error);
+    }
+
+    function limpiarError(referencia) {
+        const error = referencia.querySelector('.error');
+        if (error) {
+            referencia.removeChild(error);
+        }
+    }
+
+    function validarEmail(email) {
+        const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        return regex.test(String(email).toLowerCase());
     }
 });
