7c3d0fc941c4a512a306bb2f7e6816aa9ebab0ba
jonanv <jonan-vargas23@hotmail.com>
2025-05-23 19:08:28 -0500

    :art: (project-filter): Add arrow functions for the selects

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -31,13 +31,36 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Eventos para los selects
-marca.addEventListener('change', filtrarAutos);
+marca.addEventListener('change', (e) => {
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener('change', (e) => {
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change', (e) => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', (e) => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', (e) => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', (e) => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', (e) => {
+    datosBusqueda.color = e.target.value;
+});
+
 
 // Funciones
-function mostrarAutos() {
-    filtarAutos(autos);
-}
-
 function llenarSelect() {
     for (let i = max; i >= min; i--) {
         const opcion = document.createElement('option');
