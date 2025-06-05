ed23a1acc8199245f23a604767579aaa8982e148
jonanv <jonan-vargas23@hotmail.com>
2025-06-04 20:26:20 -0500

    :sparkles: (project-cotizator-prototypes): Create object constructors and add protorype llenarOpciones

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,2 +1,31 @@
-// Prototypes
-// app de seguros con prototypes
+// Object constructors
+function Seguro(marca, anio, tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
+
+function UI() {
+
+}
+
+// LLenar las opciones de los años
+UI.prototype.llenarOpciones = function() {
+    const year = document.querySelector('#year');
+    let max = new Date().getFullYear();
+    let min = max - 20;
+
+    for (let i = max; i >= min; i--) {
+        const option = document.createElement('option');
+        option.value = min;
+        option.textContent = i;
+        year.appendChild(option);
+    }
+}
+
+const ui = new UI();
+console.log(ui);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
