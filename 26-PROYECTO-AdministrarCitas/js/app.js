52c6155b6af6393a89a33f8889f7ec2c112a06e9
jonanv <jonan-vargas23@hotmail.com>
2025-06-13 17:57:14 -0500

    :art: (project-admin-appointment): Create class of notification

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -36,7 +36,36 @@ function enviarFormulario(e) {
     e.preventDefault();
 
     if (Object.values(citaObj).some(value => value.trim() === '')) {
-        console.log(`Todos los campos estan vacíos.`);
+        new Notificacion({
+            texto: 'Todos los campos estan vacíos.', 
+            tipo: 'error'
+        });
         return;
+    }
+}
+
+// Class
+class Notificacion {
+    constructor({texto, tipo}) {
+        this.texto = texto;
+        this.tipo = tipo;
+
+        this.mostrarNofiticacion();
+    }
+
+    mostrarNofiticacion() {
+        // Crear notificación
+        const alerta = document.createElement('div');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        // Si es de tipo error, agrega la clase
+        this.tipo === 'error' 
+            ? alerta.classList.add('bg-red-500') 
+            : alerta.classList.add('bg-green-500');
+
+        // Insertar texto
+        alerta.textContent = this.texto;
+        // Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario);
     }
 }
