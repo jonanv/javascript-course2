b0006eb8632e9a1134ced9f80595b783d1d6a729
jonanv <jonan-vargas23@hotmail.com>
2025-06-19 11:25:47 -0500

    :sparkles: (array-methods): Add method .at

---
+++ 12-ArrayMethods/js/09-app.js
@@ -0,0 +1,11 @@
+// Metodo .at
+
+const fruits = ['apple', 'banana', 'orange', 'grape'];
+
+// Using positive indexing
+console.log(fruits.at(0));  // Output: 'apple'
+console.log(fruits.at(2));  // Output: 'orange'
+
+// Using negative indexing
+console.log(fruits.at(-1)); // Output: 'grape'
+console.log(fruits.at(-3)); // Output: 'banana'
