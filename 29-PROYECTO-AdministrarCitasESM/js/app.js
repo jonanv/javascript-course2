26b859e8cb5557f6187e92ea24f34665507823ac
jonanv <jonan-vargas23@hotmail.com>
2025-06-20 12:41:12 -0500

    :art: (project-appointment-modules): Export and import class Notificacion

--- 29-PROYECTO-AdministrarCitasESM/js/app.js
+++ 29-PROYECTO-AdministrarCitasESM/js/app.js
@@ -1,3 +1,4 @@
+import Notificacion from "./classes/Notificacion.js";
 // Selectores
 const pacienteInput = document.querySelector('#paciente');
 const propietarioInput = document.querySelector('#propietario');
@@ -32,38 +33,6 @@ sintomasInput.addEventListener('change', llenarCampoCita);
 formulario.addEventListener('submit', enviarFormulario);
 
 // Class
-class Notificacion {
-    constructor({texto, tipo}) {
-        this.texto = texto;
-        this.tipo = tipo;
-
-        this.mostrarNofiticacion();
-    }
-
-    mostrarNofiticacion() {
-        // Crear notificación
-        const alerta = document.createElement('div');
-        alerta.classList.add('alert', 'text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'uppercase', 'font-bold', 'text-sm');
-
-        // Eliminar alertas duplicadas
-        const alertaPrevia = document.querySelector('.alert');
-        alertaPrevia?.remove();
-
-        // Si es de tipo error, agrega la clase
-        this.tipo === 'error' 
-            ? alerta.classList.add('bg-red-500') 
-            : alerta.classList.add('bg-green-500');
-
-        // Insertar texto
-        alerta.textContent = this.texto;
-        // Insertar en el DOM
-        formulario.parentElement.insertBefore(alerta, formulario);
-
-        setTimeout(() => {
-            alerta.remove();
-        }, 3000);
-    }
-}
 
 class AdminCitas {
     constructor() {
