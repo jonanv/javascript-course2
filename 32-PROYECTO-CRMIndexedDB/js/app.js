736b70a14b7c96d598a09a6f27df91e7e40b49c3
jonanv <jonan-vargas23@hotmail.com>
2025-06-22 20:45:26 -0500

    :art: (project-crm-indexeddb): Add list client to ui

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -3,6 +3,10 @@
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
+
+        if (window.indexedDB.open('crm', 1)) {
+            obtenerClientes();
+        }
     });
 
 
@@ -15,7 +19,6 @@
 
         crearDB.onsuccess = () => {
             DB = crearDB.result;
-            listarClientes();
         };
 
         crearDB.onupgradeneeded = (e) => {
@@ -36,28 +39,51 @@
         };
     }
 
-    function listarClientes() {
-        const objectStore = DB.transaction('crm').objectStore('crm');
+    function obtenerClientes() {
+        const abrirConexion = window.indexedDB.open('crm', 1);
 
-        // Si no hay clientes
-        const totalClientes = objectStore.count();
-        totalClientes.onsuccess = () => {
-            if (totalClientes.result === 0) {
-                return;
+        abrirConexion.onerror = () => {
+            console.error('Hubo un error');
+        };
+
+        abrirConexion.onsuccess = () => {
+            DB = abrirConexion.result;
+
+            const objectStore = DB.transaction('crm').objectStore('crm');
+    
+            // Listar clientes
+            objectStore.openCursor().onsuccess = (e) => {
+                const cursor = e.target.result;
+    
+                if (cursor) {
+                    const { id, nombre, telefono, email, empresa } = cursor.value;
+
+                    const listadoClientes = document.querySelector('#listado-clientes');
+                    listadoClientes.innerHTML += `
+                        <tr>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
+                                <p class="text-gray-700">${telefono}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
+                                <p class="text-gray-600">${empresa}</p>
+                            </td>
+                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
+                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            </td>
+                        </tr>
+                    `;
+                    
+                    // Ir al siguiente elemento
+                    cursor.continue();
+                } else {
+                    console.log('No hay más registros');
+                }
             }
-        }
-
-        // Listar clientes
-        objectStore.openCursor().onsuccess = (e) => {
-            const cursor = e.target.result;
-
-            if (cursor) {
-                
-
-                // Ir al siguiente elemento
-                cursor.continue();
-            }
-        }
-
+        };
     }
 })();
