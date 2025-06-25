8a62007eb5b01ffdd183efbd71fb5eb78ee6be59
jonanv <jonan-vargas23@hotmail.com>
2025-06-25 18:59:51 -0500

    :art: (project-weather-api): Remove duplicate alert and implement setTimeOut

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -20,12 +20,20 @@ function buscarClima(e) {
 }
 
 function mostrarError(mensaje) {
+    // Eliminar alerta duplicadas
+    alertaPrevia = document.querySelector('.alert');
+    alertaPrevia?.remove();
+
     const alerta = document.createElement('div');
-    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+    alerta.classList.add('alert', 'bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
     alerta.innerHTML = `
         <strong class='font-bold'>Error!</strong>
         <span class='block'>${ mensaje }</span>
     `;
 
     container.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
 }
