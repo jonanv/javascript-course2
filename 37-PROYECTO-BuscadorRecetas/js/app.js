dd95227f74cfe5934255f5740cd888c8e256b7dc
jonanv <jonan-vargas23@hotmail.com>
2025-06-26 16:49:51 -0500

    :art: (project-recipes): Add function to get recipes

---
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -0,0 +1,25 @@
+const urlCategorias = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const urlFiltro = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
+const urlRecepta = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
+
+
+document.addEventListener('DOMContentLoaded', iniciarApp);
+
+function iniciarApp() {
+    const PROTOCOLO = 'https://';
+    const DOMINIO = 'www.themealdb.com';
+    const PATH = '/api/json';
+    const VERSION = '/v1';
+
+    obtenerCategorias();
+
+    function obtenerCategorias() {
+        const enpoint = '/1/categories.php';
+        const URL = `${ PROTOCOLO }${ DOMINIO }${ PATH }${ VERSION }${ enpoint }`;
+
+        fetch(URL)
+            .then((response) => response.json())
+            .then(console.log)
+            .catch((error) => console.error(error));
+    }
+}
