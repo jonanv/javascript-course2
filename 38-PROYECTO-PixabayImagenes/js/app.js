4aa125019cf6505ab9d468650d69bbccd931ca15
jonanv <jonan-vargas23@hotmail.com>
2025-06-27 16:21:05 -0500

    :art: (project-pixabay-images): Remove alerts duplicate

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -17,8 +17,12 @@ function validarFormulario(e) {
 }
 
 function mostrarAlerta(mensaje) {
+    // Eliminar alertas duplicadas
+    const alertaPrevia = document.querySelector('.alerta');
+    alertaPrevia?.remove();
+
     const alerta = document.createElement('P');
-    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+    alerta.classList.add('alerta', 'bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
     alerta.innerHTML = `
         <strong class="block font-bold">Error!</strong>
         <span class="block sm:inline">${ mensaje }</span>
