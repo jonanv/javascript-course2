991c41875ed2746d28e7b2955c8bcd75d8c9f360
jonanv <jonan-vargas23@hotmail.com>
2025-06-27 16:19:07 -0500

    :art: (project-pixabay-images): Add validation for form

---
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -0,0 +1,32 @@
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFormulario);
+}
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if (terminoBusqueda.trim() === '') {
+        mostrarAlerta('Agrega un termino de búsqueda');
+        return;
+    }
+}
+
+function mostrarAlerta(mensaje) {
+    const alerta = document.createElement('P');
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+    alerta.innerHTML = `
+        <strong class="block font-bold">Error!</strong>
+        <span class="block sm:inline">${ mensaje }</span>
+    `;
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
