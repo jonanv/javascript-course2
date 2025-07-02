f47da889ea9fd861bc2af38c2dc58fbec9b508d6
jonanv <jonan-vargas23@hotmail.com>
2025-07-02 16:33:55 -0500

    :art: (project-tips): Implements function show alert

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -11,8 +11,29 @@ function guardarCliente() {
     const mesa = document.querySelector('#mesa').value;
     const hora = document.querySelector('#hora').value;
 
-    if (mesa.trim() === '' || hora.trim() === '') {
-        console.log('Los campos son obligatorios');
+    // Revisar si hay campos vacios
+    const camposVacios = [ mesa, hora ].some((campo) => campo === '');
+
+    if (camposVacios) {
+        mostrarAlerta('Los campos son obligatorios');
         return;
+    } else {
+        mostrarAlerta('Todos los campos estan llenos');
     }
+}
+
+function mostrarAlerta(mensaje) {
+    const alertaPrevia = document.querySelector('.alerta');
+    alertaPrevia?.remove();
+    
+    const alerta = document.createElement('DIV');
+    alerta.classList.add('alerta', 'invalid-feedback', 'd-block', 'text-center');
+    alerta.textContent = mensaje;
+    
+    const formulario = document.querySelector('.modal-body form');
+    formulario.append(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
 }
