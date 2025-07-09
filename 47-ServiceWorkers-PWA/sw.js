07d35887ca39e555fe31d0ba1f3adaa91deed6a5
jonanv <jonan-vargas23@hotmail.com>
2025-07-09 14:47:36 -0500

    :sparkles: (service-workers-pwa): Add fetch to service worker

--- 47-ServiceWorkers-PWA/sw.js
+++ 47-ServiceWorkers-PWA/sw.js
@@ -1,4 +1,13 @@
-// No se puede usar el objecto window
+// Para que una PWA se pueda instalar
+// 1. Tener un manifest valido
+// 2. Tener un dominio https o ser un localhost
+// 3. Tener registrado el eventListener de fetch
+
+// Manifest
+// En JavaScript, un manifiesto es un archivo en formato JSON que proporciona información sobre una aplicación web progresiva (PWA) y cómo debe ser tratada por el navegador, especialmente cuando se instala en el dispositivo del usuario. Este archivo, generalmente llamado manifest.json o site.webmanifest, describe aspectos como el nombre de la aplicación, los iconos, la orientación preferida, el color del tema, la URL de inicio y otros metadatos relevantes. 
+
+
+// No se puede usar el objecto window por eso se usa self
 
 // Cuando se instala el service worker, solo se ejecuta una sola vez hasta que es instalado
 self.addEventListener('install', (e) => {
@@ -10,4 +19,9 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
     console.log('Service worker activado');
     console.log(e);
+});
+
+// Evento fetch para descargar archivos estaticos
+self.addEventListener('fetch', (e) => {
+    console.log('Fetch...', e);
 });
