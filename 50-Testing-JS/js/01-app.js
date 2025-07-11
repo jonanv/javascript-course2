1624be5111dfa79a5581c6f7822c5a8813131b19
jonanv <jonan-vargas23@hotmail.com>
2025-07-11 15:24:41 -0500

    :art: (testing): Add another test

--- 50-Testing-JS/js/01-app.js
+++ 50-Testing-JS/js/01-app.js
@@ -4,8 +4,21 @@ function suma(a, b) {
     return a + b;
 }
 
-const resultado = suma(1, 2);
-const esperado = 3;
+function resta(a, b) {
+    return a - b;
+}
+
+let resultado = suma(1, 2);
+let esperado = 3;
+
+if (resultado !== esperado) {
+    console.error(`El resultado: ${ resultado } es diferente a lo esperado; la prueba no pasó`);
+} else {
+    console.log('La prueba paso correctamente');
+}
+
+resultado = resta(10, 7);
+esperado = 5;
 
 if (resultado !== esperado) {
     console.error(`El resultado: ${ resultado } es diferente a lo esperado; la prueba no pasó`);
