0092170b04ce26f0bd23bbd2e0d47e371ffb8dfe
jonanv <jonan-vargas23@hotmail.com>
2025-07-13 14:36:44 -0500

    :white_check_mark: (testing-jest): Add testing for update and delete appointment

--- 51-Testing-Jest/__tests__/citas.js
+++ 51-Testing-Jest/__tests__/citas.js
@@ -1,11 +1,13 @@
 import Citas from "../js/classes/Citas";
 
-describe('Probar la calse de citas', () => {
+describe('Probar la clase de citas', () => {
 
     const citas = new Citas();
+    const id = Date.now();
 
     test('Agregar una nueva cita', () => {
         const citaObj = {
+            id,
             mascota: 'Hook',
             propietario: 'Juan',
             telefono: '32323',
@@ -14,9 +16,35 @@ describe('Probar la calse de citas', () => {
             sintomas: 'Duermen'
         };
 
-        citaObj.id = Date.now();
-
         citas.agregarCita(citaObj);
+
+        // Prueba
+        expect(
+            citas
+        ).toMatchSnapshot();
+    });
+
+    test('Actualizar cita', () => {
+        const citaObjActualizada = {
+            id,
+            mascota: 'Nuevo nombre',
+            propietario: 'Juan',
+            telefono: '32323',
+            fecha: '10-12-2025',
+            hora:'10:30',
+            sintomas: 'Duermen'
+        };
+
+        citas.editarCita(citaObjActualizada);
+
+        // Prueba
+        expect(
+            citas
+        ).toMatchSnapshot();
+    });
+
+    test('Eliminar cita', () => {
+        citas.eliminarCita(id);
 
         // Prueba
         expect(
