0462f727f1cddecdd6acffc5bee8f97b559a1b19
jonanv <jonan-vargas23@hotmail.com>
2025-07-15 11:14:24 -0500

    :white_check_mark: (testing-cypress): Write into form and create a new appointment

---
+++ 52-Testing-Cypress/cypress/e2e/crear_cita.spect.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+describe('Name of the group', () => {
+    it('should ', () => {
+        cy.visit('/index.html');
+
+        cy.get('[data-cy="mascota-input"]')
+            .type('Hook');
+        cy.get('[data-cy="propietario-input"]')
+            .type('Juan');
+        cy.get('[data-cy="telefono-input"]')
+            .type('33333');
+        cy.get('[data-cy="fecha-input"]')
+            .type('2025-07-15');
+        cy.get('[data-cy="hora-input"]')
+            .type('10:30');
+        cy.get('[data-cy="sintomas-input"]')
+            .type('Duerme mucho');
+        cy.get('[data-cy="submit-cita"]')
+            .click();
+
+        cy.get('[data-cy=citas-heading]')
+            .invoke('text')
+            .should('equal', 'Administra tus Citas');
+
+        cy.get('[data-cy="alerta"]')
+            .invoke('text')
+            .should('equal', 'Se agregó correctamente');
+
+        cy.get('[data-cy="alerta"]')
+            .should('have.class', 'alert-success');
+
+    });
+});
