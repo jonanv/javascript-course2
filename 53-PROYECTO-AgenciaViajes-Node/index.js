1396148179bdf93f9073c02a2dee1e7c9375952a
jonanv <jonan-vargas23@hotmail.com>
2025-07-15 16:22:55 -0500

    :sparkles: (project-travel-agency-node): Add  routes with send, json and render

--- 53-PROYECTO-AgenciaViajes-Node/index.js
+++ 53-PROYECTO-AgenciaViajes-Node/index.js
@@ -9,6 +9,26 @@ const app = express();
 // Definir puerto
 const port = process.env.PORT || 4000;
 
+// req - lo que enviamos
+// res - lo que express nos responde
+app.get('/', (request, response) => {
+    response.send('Inicio');
+
+    // response.json({
+    //     id: 1
+    // });
+
+    // response.render();
+});
+
+app.get('/nosotros', (request, response) => {
+    response.send('Nosotros');
+});
+
+app.get('/Contacto', (request, response) => {
+    response.send('Contacto');
+});
+
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${ port }`);
 });
