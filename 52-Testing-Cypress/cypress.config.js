3ef1fc440b2a8e83a51b5ac1e8e8401e5e469463
jonanv <jonan-vargas23@hotmail.com>
2025-07-15 14:11:39 -0500

    :wrench: (testing-cypress): Add config for cypress

--- 52-Testing-Cypress/cypress.config.js
+++ 52-Testing-Cypress/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://127.0.0.1:5500/52-Testing-Cypress/'
-  },
+    baseUrl: 'http://127.0.0.1:5500/52-Testing-Cypress/',
+    testIsolation: false
+  }
 });
