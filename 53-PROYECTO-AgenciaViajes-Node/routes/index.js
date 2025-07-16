003e79c0e87a9d80599c0ac328b395097e78a180
jonanv <jonan-vargas23@hotmail.com>
2025-07-16 14:40:16 -0500

    :sparkles: (project-travel-agency-node): Create layout and implement into page home and ours

--- 53-PROYECTO-AgenciaViajes-Node/routes/index.js
+++ 53-PROYECTO-AgenciaViajes-Node/routes/index.js
@@ -5,14 +5,11 @@ const router = express.Router();
 // req - lo que enviamos
 // res - lo que express nos responde
 router.get('/', (request, response) => {
-    response.send('Inicio');
+    response.render('inicio.pug');
 });
 
 router.get('/nosotros', (request, response) => {
-    const viajes = 'Viaje a Alemania';
-    response.render('nosotros.pug', {
-        viajes
-    });
+    response.render('nosotros.pug');
 });
 
 export default router;
