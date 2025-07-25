d2ba9f5cb013bb123dd06cfc50e47140bc8bf599
jonanv <jonan-vargas23@hotmail.com>
2025-07-25 11:10:35 -0500

    :sparkles: (project-loan-calculator-react): Create component header to app

--- 54-PROYECTO-CotizadorPrestamos-React/src/App.jsx
+++ 54-PROYECTO-CotizadorPrestamos-React/src/App.jsx
@@ -1,10 +1,10 @@
-function App() {
+import Header from "./components/Header"
 
-  const hola = 'Hola Mundo';
+function App() {
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
-      { hola }
+      <Header />
     </div>
   )
 }
