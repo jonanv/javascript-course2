ebe46e498b5d6c944f8233077510973df7cf70f6
jonanv <jonan-vargas23@hotmail.com>
2025-07-25 16:59:19 -0500

    :sparkles: (project-loan-calculator-react): Implement useEffect

--- 54-PROYECTO-CotizadorPrestamos-React/src/App.jsx
+++ 54-PROYECTO-CotizadorPrestamos-React/src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 // Components
 import Header from "./components/Header"
 import Button from "./components/Button"
-import { formatearDinero } from "./helpers/index";
+import { formatearDinero, calcularTotalPagar } from "./helpers/index";
 
 function App() {
     const [cantidad, setCantidad] = useState(10000);
     const [meses, setMeses] = useState(6);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+        setTotal(resultadoTotalPagar);
+    }, [cantidad, meses]);
 
     const MIN = 0;
     const MAX = 20000;
@@ -69,6 +75,16 @@ function App() {
                 <option value="24">24 Meses</option>
                 <option value="48">48 Meses</option>
             </select>
+
+            <div className='my-5 space-y-3 bg-gray-50 p-50'>
+                <h2 className='text-2xl font-extrabold text-gray-500 text-center'>
+                    Resumen <span className='text-indigo-600'>de pagos</span>
+                </h2>
+
+                <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+                <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total)} Total a pagar</p>
+                <p className="text-xl text-gray-500 text-center font-bold">{formatearDinero(total / meses)} Mensuales</p>
+            </div>
         </div>
     )
 }
--- 54-PROYECTO-CotizadorPrestamos-React/src/helpers/index.js
+++ 54-PROYECTO-CotizadorPrestamos-React/src/helpers/index.js
@@ -7,6 +7,35 @@ const formatearDinero = (valor) => {
     return formatter.format(valor);
 }
 
+const calcularTotalPagar = (cantidad, plazo) => {
+    let total;
+
+    // Mientras mayor es la cantidad solictada, menor es el interes
+    if (cantidad < 5000) {
+        total = cantidad * 1.5;
+    } else if (cantidad >= 5000 && cantidad < 10000) {
+        total = cantidad * 1.4;
+    } else if (cantidad >= 10000 && cantidad < 15000) {
+        total = cantidad * 1.3;
+    } else {
+        total = cantidad * 1.2;
+    }
+
+    // plazo - mas plazo, mayor interes
+    if (plazo === 6) {
+        total *= 1.1;
+    } else if (plazo === 12) {
+        total *= 1.2;
+    } else if (plazo === 24) {
+        total *= 1.3;
+    } else {
+        total *= 1.4;
+    }
+
+    return total;
+}
+
 export {
-    formatearDinero
+    formatearDinero,
+    calcularTotalPagar
 }
