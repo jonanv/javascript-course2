9f98da04f63fddce4ca3a780bc8daba15183c462
jonanv <jonan-vargas23@hotmail.com>
2025-07-31 20:24:15 -0500

    :sparkles: (project-loan-calculator-vue): Create a component in Vue

--- 55-PROYECTO-CotizadorPrestamos-Vue/src/App.vue
+++ 55-PROYECTO-CotizadorPrestamos-Vue/src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+    import Header from './components/Header.vue'
 </script>
 
 <template>
-  <h1>Hola mundo</h1>
+    <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+        <Header />
+    </div>
 </template>
-
