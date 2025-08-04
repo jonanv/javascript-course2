dd85b1286928d98df2520c7aa0005fa3878f6c5a
jonanv <jonan-vargas23@hotmail.com>
2025-08-03 19:01:46 -0500

    :sparkles: (project-loan-calculator-vue): Manage state with ref and reactive into Vue

--- 55-PROYECTO-CotizadorPrestamos-Vue/src/App.vue
+++ 55-PROYECTO-CotizadorPrestamos-Vue/src/App.vue
@@ -1,9 +1,36 @@
 <script setup>
-    import Header from './components/Header.vue'
+    import { ref, reactive } from 'vue'; // la forma de manejar el estado en vue ref maneja los primitivos y reactive los objetos
+    import Header from './components/Header.vue';
+
+    const cantidad = ref(0);
+    const state = reactive({
+        cantidad: 0
+    });
+
+    console.log(cantidad.value);
+    console.log(state.cantidad);
+    
+    
+
+    function handleChange(e) {
+        console.log(e.target.value);
+    }
 </script>
 
 <template>
     <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
+
+        <div class="my-5">
+            <input 
+                type="range"
+                class="w-full bg-gray-500 accent-lime-500 hover:accent-lime-600"
+                v-on:input="handleChange"
+                min="0"
+                max="20000"
+                step="100"
+                value="10000"
+            />
+        </div>
     </div>
 </template>
