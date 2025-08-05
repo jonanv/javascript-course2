b88ad5783200d719172fc7b9c94032ecd39312ea
jonanv <jonan-vargas23@hotmail.com>
2025-08-05 18:31:38 -0500

    :sparkles: (project-loan-calculator-vue): Add buttons increment and decrement

--- 55-PROYECTO-CotizadorPrestamos-Vue/src/App.vue
+++ 55-PROYECTO-CotizadorPrestamos-Vue/src/App.vue
@@ -14,11 +14,32 @@
         });
         return formatter.format(cantidad.value);
     });
+
+    const handlerChangeDecremento = () => {
+        cantidad.value = Math.max(cantidad.value - STEP, MIN);
+    }
+
+    const handlerChangeIncremento = () => {
+        cantidad.value = Math.min(cantidad.value + STEP, MAX);
+    }
 </script>
 
 <template>
     <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
         <Header />
+
+        <div class="flex justify-between mt-10">
+            <button 
+                class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+                @click="handlerChangeDecremento">
+            -
+            </button>
+            <button 
+                class="h-10 w-10 flex items-center justify-center font-bold bg-lime-500 rounded-full hover:outline-none hover:ring-2 hover:ring-offset-2 hover:ring-lime-500 text-white text-2xl"
+                @click="handlerChangeIncremento">
+            +
+            </button>
+        </div>
 
         <div class="my-5">
             <input 
