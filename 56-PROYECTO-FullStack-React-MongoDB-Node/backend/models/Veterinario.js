c5fe5b9b15223827c658ac6da53cd9d443d8bf88
jonanv <jonan-vargas23@hotmail.com>
2025-08-11 17:42:13 -0500

    :art: (project-fullstack): Add fields to model Veterinario

--- 56-PROYECTO-FullStack-React-MongoDB-Node/backend/models/Veterinario.js
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/models/Veterinario.js
@@ -13,6 +13,27 @@ const veterinarioSchema = mongoose.Schema({
     email: {
         type: String,
         require: true,
-        unique: true
+        unique: true,
+        trim: true
+    },
+    telefono: {
+        type: String,
+        default: null,
+        trim: true
+    },
+    web: {
+        type: String,
+        default: null
+    },
+    token: {
+        type: String
+    },
+    confirmado: {
+        type: Boolean,
+        default: false
     }
 });
+
+const Veterinario = mongoose.model('Veterinario', veterinarioSchema);
+
+export default Veterinario;
