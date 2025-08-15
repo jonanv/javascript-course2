2604e4fe5e55442f17a3b6215c2ed5e255b06750
jonanv <jonan-vargas23@hotmail.com>
2025-08-14 20:15:23 -0500

    :sparkles: (project-fullstack): Create new veterinario in database

--- 56-PROYECTO-FullStack-React-MongoDB-Node/backend/controllers/veterinarioController.js
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/controllers/veterinarioController.js
@@ -1,6 +1,17 @@
-const registrar = (require, response) => {
-    console.log(require.body);
-    response.json({ msg: 'Registrando usuario...' });
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (require, response) => {
+    const { nombre, email, password } = require.body;
+
+    try {
+        // Guardar un nuevo Veterinario
+        const veterinario = new Veterinario(require.body);
+        const veterinarioGuardado = await veterinario.save();
+
+        response.json(veterinarioGuardado);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const perfil = (require, response) => {
