ec8a1ae1c302b98c0ca250bc6e5406e6563ae51f
jonanv <jonan-vargas23@hotmail.com>
2025-08-15 21:38:13 -0500

    :art: (project-fullstack): Update model Veterinario

--- 56-PROYECTO-FullStack-React-MongoDB-Node/backend/models/Veterinario.js
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/models/Veterinario.js
@@ -26,7 +26,8 @@ const veterinarioSchema = mongoose.Schema({
         default: null
     },
     token: {
-        type: String
+        type: String,
+        default: Date.now()
     },
     confirmado: {
         type: Boolean,
