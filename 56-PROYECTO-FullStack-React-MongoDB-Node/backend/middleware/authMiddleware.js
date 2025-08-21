ddc3c22d3bea80f7a3ea618b4da4a2d525c153cd
jonanv <jonan-vargas23@hotmail.com>
2025-08-20 19:17:55 -0500

    :sparkles: (project-fullstack): Implement JWT and verify token into middleware

--- 56-PROYECTO-FullStack-React-MongoDB-Node/backend/middleware/authMiddleware.js
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/middleware/authMiddleware.js
@@ -1,11 +1,30 @@
-const checkAuth = (request, response, next) => {
+import jwt from "jsonwebtoken";
+import Veterinario from "../models/Veterinario.js";
+
+const checkAuth = async (request, response, next) => {
+    let token;
+
     if (request.headers.authorization && 
         request.headers.authorization.startsWith('Bearer')) {
-        next();
+        try {
+            token = request.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            request.veterinario = await Veterinario.findById(decoded.id).select(
+                '-password -token -confirmado'
+            );
+            return next();
+        } catch (error) {
+            const customError = new Error('Token no válido');
+            response.status(403).json({ message: customError.message });
+        }
     }
 
-    const error = new Error('Token no válido o inexistente');
-    response.status(403).json({ message: error.message });
+    if (!token) {
+        const error = new Error('Token no válido o inexistente');
+        response.status(403).json({ message: error.message });
+    }
+
+    next();
 };
 
 export default checkAuth;
