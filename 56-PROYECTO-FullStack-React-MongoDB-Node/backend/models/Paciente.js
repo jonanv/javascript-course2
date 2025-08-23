1fb1786d41a251a6a512c41baf315bd9e7db4787
jonanv <jonan-vargas23@hotmail.com>
2025-08-22 19:31:11 -0500

    :sparkles: (project-fullstack): Create model of Paciente

---
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/models/Paciente.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const pacienteSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    propietario: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    fecha: {
+        type: Date,
+        required: true
+    },
+    sintomas: {
+        type: String,
+        required: true
+    },
+    veterinario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Veterinario'
+    }
+}, {
+    timestamps: true // Crea las columnas de editado y de creado
+});
+
+const Paciente = mongoose.model('Paciente', pacienteSchema);
+
+export default Paciente;
