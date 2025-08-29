fe692bf15b53c12b88737ba16c0d73f3b9990151
jonanv <jonan-vargas23@hotmail.com>
2025-08-29 17:37:51 -0500

    :lipstick: (project-fullstack): Create formulario with tailwindcss

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/Login.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/Login.jsx
@@ -7,7 +7,42 @@ const Login = () => {
                 </h1>
             </div>
             <div>
-                2
+                <form className="my-5">
+                    <div>
+                        <label 
+                            htmlFor="email"
+                            className="uppercase text-gray-600 block text-xl font-bold">
+                            Email
+                        </label>
+                        <input 
+                            type="email"
+                            placeholder="Email de Registro"
+                            name="email" 
+                            id="" 
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        />
+                    </div>
+                    <div>
+                        <label 
+                            htmlFor="password"
+                            className="uppercase text-gray-600 block text-xl font-bold">
+                            Password
+                        </label>
+                        <input 
+                            type="password"
+                            placeholder="Tu Password"
+                            name="password" 
+                            id="" 
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        />
+                    </div>
+                    <input 
+                        type="submit" 
+                        value="Iniciar Sesión"
+                        className="bg-indigo-700 w-full text-white uppercase font-bold border rounded-xl py-3 px-10 mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+
+                    />
+                </form>
             </div>
         </>
     );
