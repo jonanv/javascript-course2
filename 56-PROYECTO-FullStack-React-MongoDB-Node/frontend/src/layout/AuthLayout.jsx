ee6d4ff65e3ccd5fdb7e898ea3a6e793035eab4e
jonanv <jonan-vargas23@hotmail.com>
2025-08-29 17:19:29 -0500

    :lipstick: (project-fulstack): Add styles for login

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/layout/AuthLayout.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/layout/AuthLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
     return (
         <>
-            <h1>Administrador de Pacientes de Veterinario</h1>
-
-            <Outlet />
+            <main className="container mx-auto md:grid md:grid-cols-2 mt-12">
+                <Outlet />
+            </main>
         </>
     )
 }
