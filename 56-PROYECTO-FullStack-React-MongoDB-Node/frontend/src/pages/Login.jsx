76426320452601bb3d23f4ab8d5f4fa7631ac636
jonanv <jonan-vargas23@hotmail.com>
2025-08-29 19:03:54 -0500

    :sparkles: (project-fullstack): Add page of forget password and routing complete

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/Login.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/Login.jsx
@@ -5,7 +5,8 @@ const Login = () => {
         <>
             <div>
                 <h1 className="text-indigo-600 font-black text-6xl">
-                    Inicia sesión y Administra tus <span className="text-black">Pacientes</span>
+                    Inicia sesión y Administra tus {""}
+                    <span className="text-black">Pacientes</span>
                 </h1>
             </div>
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/Registrar.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/Registrar.jsx
@@ -5,7 +5,8 @@ const Registrar = () => {
         <>
             <div>
                 <h1 className="text-indigo-600 font-black text-6xl">
-                    Crea tu cuenta y Administra <span className="text-black">tus Paciente</span>
+                    Crea tu cuenta y Administra {""}
+                    <span className="text-black">tus Paciente</span>
                 </h1>
             </div>
             <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
