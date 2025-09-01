dd8d8b1b361dcb9b86a862d0f8e405dec70b9cdd
jonanv <jonan-vargas23@hotmail.com>
2025-09-01 18:50:06 -0500

    :sparkles: (project-fullstack): Add variables to config send email

---
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/helpers/emailRegistro.js
@@ -0,0 +1,15 @@
+import nodemailer from "nodemailer";
+
+const emailRegistro = () => {
+    // Looking to send emails in production? Check out our Email API/SMTP product!
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+};
+
+export default emailRegistro;
