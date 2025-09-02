1b8f608642fbf4f80cccec9da712bce34d97c94a
jonanv <jonan-vargas23@hotmail.com>
2025-09-01 19:20:56 -0500

    :sparkles: (project-fullstack): Implement send email ahd recibe with mailtrap

--- 56-PROYECTO-FullStack-React-MongoDB-Node/backend/helpers/emailRegistro.js
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/backend/helpers/emailRegistro.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 
-const emailRegistro = () => {
+const emailRegistro = async (datos) => {
     // Looking to send emails in production? Check out our Email API/SMTP product!
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
@@ -10,6 +10,25 @@ const emailRegistro = () => {
             pass: process.env.EMAIL_PASS
         }
     });
+
+    const { email, nombre, token } = datos;
+    // Enviar el email
+    const info = await transporter.sendMail({
+        from: 'APV - Administrado de Pacientes de Veterinaria',
+        to: email,
+        subject: 'Comprueba tu cuenta en APV',
+        text: 'Comprueba tu cuenta en APV',
+        html: `
+            <p>Hola, ${nombre}, comprueba tu cuenta APV</p>
+            <p>Tu cuenta ya esta lista, solo debes de comprobarla en el siguiente enlace:
+                <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a>
+            </p>
+
+            <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje</p>
+        `
+    });
+
+    console.log('Mensaje enviado: %s', info.messageId);
 };
 
 export default emailRegistro;
