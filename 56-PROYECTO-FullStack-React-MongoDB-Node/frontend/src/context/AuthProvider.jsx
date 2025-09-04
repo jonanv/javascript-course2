bbdf40d417143139a2dbf94c1f69baea2b874199
jonanv <jonan-vargas23@hotmail.com>
2025-09-04 18:01:06 -0500

    :sparkles: (project-fullstack): Create custom hooks and access to global state of context

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/AuthProvider.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/AuthProvider.jsx
@@ -4,9 +4,13 @@ const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
-    
+
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider
+            value={{
+                auth, 
+                setAuth
+            }}>
             { children }
         </AuthContext.Provider>
     );
