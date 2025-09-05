a4a1917c646e08ed137479616273709ec7093a11
jonanv <jonan-vargas23@hotmail.com>
2025-09-05 11:22:33 -0500

    :sparkles: (project-fullstack): Auth user with JWT

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/AuthProvider.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,34 @@
 import { useState, useEffect, createContext } from 'react';
+import clienteAxios from '../config/axios';
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
+
+    useEffect(() => {
+        const autenticarUsuario = async () => {
+            const token = localStorage.getItem('token');
+            
+            if (!token) return;
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${ token }`
+                }
+            }
+
+            try {
+                const { data } = await clienteAxios.get('/veterinarios/perfil', config);
+                setAuth(data);
+            } catch (error) {
+                console.error(error.response.data.message);
+                setAuth({});
+            }
+        };
+        autenticarUsuario();
+    }, []);
 
     return (
         <AuthContext.Provider
