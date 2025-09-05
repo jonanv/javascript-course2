b2d8514b8845a0a09061a86f88b96d51590aab56
jonanv <jonan-vargas23@hotmail.com>
2025-09-05 15:13:56 -0500

    :sparkles: (project-fullstack): Add props color to Loading component

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/components/Loading.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/components/Loading.jsx
@@ -1,8 +1,8 @@
-const Loading = () => {
+const Loading = ({ color }) => {
     return (
         <>
             <svg
-                className="animate-spin h-5 w-5 text-white"
+                className={`${ color ? color : 'text-white' } animate-spin h-5 w-5`}
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
