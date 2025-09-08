7ee68dbe2d3667bc0854bc62bb86782dc4dc9f38
jonanv <jonan-vargas23@hotmail.com>
2025-09-08 18:47:11 -0500

    :art: (project-fullstack): Create ListadoPaciente component and show and hide form

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/AdministrarPacientes.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/pages/AdministrarPacientes.jsx
@@ -1,10 +1,28 @@
-import Footer from "../components/Footer";
+import { useState } from "react";
+
+// Imports
+import Formulario from "../components/Formulario";
+import ListadoPacientes from "../components/ListadoPacientes";
 
 const AdministrarPacientes = () => {
+    const [showForm, setShowForm] = useState(false);
+
     return (
-        <>
-            <div>Desde Administrador de Pacientes</div>
-        </>
+        <div className="flex flex-col md:flex-row">
+            <button 
+                type="button"
+                className="bg-indigo-600 text-white font-bold uppercase mx-10 p-3 rounded-md mb-10 md:hidden"
+                onClick={() => setShowForm(!showForm)}>
+                {showForm ? 'Ocultar formulario' : 'Mostrar formulario'}
+            </button>
+            
+            <div className={`${ showForm ? 'block' : 'hidden' } md:block md:w-1/2 lg:w-2/5`}>
+                <Formulario />
+            </div>
+            <div className="md:w-1/2 lg:w-3/5">
+                <ListadoPacientes />
+            </div>
+        </div>
     );
 }
 
