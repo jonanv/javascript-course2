15ecb110e2d9d52bb1b441ab7f6def92cf320318
jonanv <jonan-vargas23@hotmail.com>
2025-09-08 20:50:09 -0500

    :sparkles: (project-fullstack): Add function of obtenerPacientes into useEffect

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/PacientesProvider.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/PacientesProvider.jsx
@@ -10,7 +10,34 @@ const PacientesProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        const obtenerPacientes = async () => {
+            const token = localStorage.getItem('token');
+            
+            if (!token) {
+                setLoading(false);
+                return;
+            }
 
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${ token }`
+                }
+            }
+
+            try {
+                const { data } = await clienteAxios.get('/pacientes/', config);
+                setPacientes(data);
+            } catch (error) {
+                console.log(error.response.data.message);
+                setPacientes([]);
+            } finally {
+                setTimeout(() => {
+                    setLoading(false);
+                }, 3000);
+            }
+        }
+        obtenerPacientes();
     }, []);
 
     const guardarPaciente = async (paciente) => {
