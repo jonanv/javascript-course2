868850041e712781a011cadaa86198966c75e537
jonanv <jonan-vargas23@hotmail.com>
2025-09-08 21:03:07 -0500

    :sparkles: (project-fullstack): Create structure for ListadoPacientes component

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/components/ListadoPacientes.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/components/ListadoPacientes.jsx
@@ -1,6 +1,38 @@
+// Imports
+import usePacientes from "../hooks/usePacientes";
+
 const ListadoPacientes = () => {
+
+    const { pacientes } = usePacientes();
+    console.log(pacientes);
+
     return (
-        <div>ListadoPacientes</div>
+        <>
+            {pacientes.length 
+                ? 
+                    (
+                        <>
+                            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+
+                            <p className="text-xl mt-5 mb-10 text-center">
+                                Administra tus {''}
+                                <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+                            </p>
+                        </>
+                    )
+                : 
+                    (
+                        <>
+                            <h2 className="font-black text-3xl text-center">No Hay Pacientes</h2>
+
+                            <p className="text-xl mt-5 mb-10 text-center">
+                                Comienza agregando pacientes {''}
+                                <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+                            </p>
+                        </>
+                    )
+            }
+        </>
     );
 }
 
