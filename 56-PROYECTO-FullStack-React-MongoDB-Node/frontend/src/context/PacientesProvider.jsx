8a64dc28f99d9249f3ae01cc7a81bdad6fbfd3c3
jonanv <jonan-vargas23@hotmail.com>
2025-09-09 11:27:42 -0500

    :art: (project-fulstack): Create function for redux code duplicate with config into context PacienteProvider

--- 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/PacientesProvider.jsx
+++ 56-PROYECTO-FullStack-React-MongoDB-Node/frontend/src/context/PacientesProvider.jsx
@@ -11,22 +11,8 @@ const PacientesProvider = ({ children }) => {
 
     useEffect(() => {
         const obtenerPacientes = async () => {
-            const token = localStorage.getItem('token');
-            
-            if (!token) {
-                setLoading(false);
-                return;
-            }
-
-            const config = {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${ token }`
-                }
-            }
-
             try {
-                const { data } = await clienteAxios.get('/pacientes/', config);
+                const { data } = await clienteAxios.get('/pacientes/', cargarConfig());
                 setPacientes(data);
             } catch (error) {
                 console.log(error.response.data.message);
@@ -41,10 +27,25 @@ const PacientesProvider = ({ children }) => {
     }, []);
 
     const guardarPaciente = async (paciente) => {
+        try {
+            const { data } = await clienteAxios.post('/pacientes', paciente, cargarConfig());
+            const { createdAt, updatedAt, __v, ...pacienteAlmacenado } = data; // Crea un nuevo objeto sin los valores de la izquierda
+            setPacientes([pacienteAlmacenado, ...pacientes]);
+        } catch (error) {
+            console.log(error.response.data.message);
+            setPacientes([]);
+        } finally {
+            setTimeout(() => {
+                setLoading(false);
+            }, 3000);
+        }
+    }
 
     const editarPaciente = async (paciente) => {
         console.log(paciente);
     }
+
+    const cargarConfig = () => {
         const token = localStorage.getItem('token');
             
         if (!token) {
@@ -59,18 +60,7 @@ const PacientesProvider = ({ children }) => {
             }
         }
 
-        try {
-            const { data } = await clienteAxios.post('/pacientes', paciente, config);
-            const { createdAt, updatedAt, __v, ...pacienteAlmacenado } = data; // Crea un nuevo objeto sin los valores de la izquierda
-            setPacientes([pacienteAlmacenado, ...pacientes]);
-        } catch (error) {
-            console.log(error.response.data.message);
-            setPacientes([]);
-        } finally {
-            setTimeout(() => {
-                setLoading(false);
-            }, 3000);
-        }
+        return config;
     }
 
     return (
