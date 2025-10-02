56adb620081bc837f3243a2415a6c6173771a2c5
jonanv <jonan-vargas23@hotmail.com>
2025-10-02 11:13:29 -0500

    :sparkles: (project-appointment-typescript): Add primitive types and interfaces typescript

--- 57-PROYECTO-AdministrarCitas-TypeScript/src/variables.ts
+++ 57-PROYECTO-AdministrarCitas-TypeScript/src/variables.ts
@@ -19,3 +19,29 @@ export {
     editando,
     citaObj
 }
+
+
+// TypeScript
+// Primitives types
+// let precio: number;
+// let nombre: string;
+// let disponible: boolean;
+
+// precio = 200;
+// nombre = 'Monitor 20 Pulgadas';
+// disponible = true;
+
+
+// interface Producto {
+//     precio: number;
+//     nombre: string;
+//     disponible: boolean;
+// }
+
+// const producto: Producto = {
+//     precio: 300,
+//     nombre: 'Monitor 24 Pulgadas',
+//     disponible: true
+// }
+
+// console.log(producto.nombre);
