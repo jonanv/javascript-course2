cc16e01b6453c79ce44a84f42517b613b4b7e037
jonanv <jonan-vargas23@hotmail.com>
2025-10-08 15:15:02 -0500

    :art: (project-appointment-typescript): Add Index Signature to allow dinamic keys

--- 57-PROYECTO-AdministrarCitas-TypeScript/src/variables.ts
+++ 57-PROYECTO-AdministrarCitas-TypeScript/src/variables.ts
@@ -1,6 +1,7 @@
 import { generarId } from "./funciones";
 
 interface Cita {
+    [key: string]: string; // Index signature para permitir claves dinámicas
     id: string;
     paciente: string;
     propietario: string;
