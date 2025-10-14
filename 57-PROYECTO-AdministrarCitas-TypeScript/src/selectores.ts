e61f0ba38ce0fbbb3039893a97a90b87b8a61e01
jonanv <jonan-vargas23@hotmail.com>
2025-10-13 22:25:40 -0500

    :art: (project-appointment-typescript): Change type for selectores

--- 57-PROYECTO-AdministrarCitas-TypeScript/src/selectores.ts
+++ 57-PROYECTO-AdministrarCitas-TypeScript/src/selectores.ts
@@ -1,9 +1,9 @@
 // Selectores
-export const pacienteInput = document.querySelector<HTMLInputElement>('#paciente');
-export const propietarioInput = document.querySelector<HTMLInputElement>('#propietario');
-export const emailInput = document.querySelector<HTMLInputElement>('#email');
-export const fechaInput = document.querySelector<HTMLInputElement>('#fecha');
-export const sintomasInput = document.querySelector<HTMLTextAreaElement>('#sintomas');
+export const pacienteInput = document.querySelector('#paciente') as HTMLInputElement;
+export const propietarioInput = document.querySelector('#propietario') as HTMLInputElement;
+export const emailInput = document.querySelector('#email') as HTMLInputElement;
+export const fechaInput = document.querySelector('#fecha') as HTMLInputElement;
+export const sintomasInput = document.querySelector('#sintomas') as HTMLTextAreaElement;
 
 export const formulario = document.querySelector<HTMLFormElement>('#formulario-cita');
 export const btnFormulario = document.querySelector<HTMLInputElement>('#formulario-cita input[type="submit"]');
