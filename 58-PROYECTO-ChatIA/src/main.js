d062da2910ebeb99f92c0dfefa14137ed95730e9
jonanv <jonan-vargas23@hotmail.com>
2025-10-24 15:22:39 -0500

    :sparkles: (project-chat-ai): Add validation to prompt

--- 58-PROYECTO-ChatIA/src/main.js
+++ 58-PROYECTO-ChatIA/src/main.js
@@ -1,2 +1,16 @@
 import './style.css'
 
+const form = document.querySelector('#prompt-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const prompt = document.querySelector('#prompt').value;
+    
+    if (prompt.trim() === '') {
+        alert('La consulta no puede ir vacía');
+        return;
+    }
+
+    console.log(prompt);
+});
